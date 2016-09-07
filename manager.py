7f180744f2484c15a4cfdbabe185b2cab688a225
ay27 <ay27@ay27deMacBook-Air.local>
2016-09-07 22:17:05 +0800

    回首两情萧索

--- manager.py
+++ manager.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     vv = vars(args)
-    # print(vv)
+    print(vv)
     if 'desktop_action' in vv.keys():
         if len(sys.argv) < 3:
             desktop_parser.print_help()
