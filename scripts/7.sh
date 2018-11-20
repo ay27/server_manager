1466d5fa367c27e71bd01fa528abcd861ae2da26
ay27 <jinmian.y@gmail.com>
2018-11-20 16:23:31 +0800

    version 7

---
+++ scripts/7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+current_version=7
+
+echo "running update version "${current_version}
+
+# 安装新的脚本
+rm /usr/bin/manager
+cp manager.py /usr/bin/manager
+chmod +x /usr/bin/manager
+
+
+# 删除/root/.server_manager
+rm -rf /root/.server_manager
+
+echo ${current_version} > /root/.manager_version
